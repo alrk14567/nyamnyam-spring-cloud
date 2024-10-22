2eaee1b70043e319b1aea375f33010159177a1ad
alrk14567 <cdutik@gmail.com>
2024-10-22 18:25:31 +0900

    prod로 변경

--- service/chat-service/src/main/java/kr/chat/config/WebFluxConfig.java
+++ service/chat-service/src/main/java/kr/chat/config/WebFluxConfig.java
@@ -3,7 +3,6 @@ package kr.chat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
