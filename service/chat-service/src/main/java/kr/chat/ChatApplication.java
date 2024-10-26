d16bb95cf100d62b47497cd92b2f9b1915546592
alrk14567 <cdutik@gmail.com>
2024-10-27 02:25:24 +0900

    test1

--- service/chat-service/src/main/java/kr/chat/ChatApplication.java
+++ service/chat-service/src/main/java/kr/chat/ChatApplication.java
@@ -3,9 +3,9 @@ package kr.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@EnableReactiveMongoRepositories
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ChatApplication {
